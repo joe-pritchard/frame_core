ff487c59b5cdf75430a022983b1254ff153935da
Joe Pritchard <joe@jpr.software>
2025-11-26 14:22:56 +0000

    Fix device ID so it does not regenerate a new ID if the file cannot be read

--- lib/frame_core.ex
+++ lib/frame_core.ex
@@ -2,7 +2,10 @@ defmodule FrameCore do
   @moduledoc """
   Supervisor for `FrameCore` Will orchestrate the DeviceId, Backend, and Slideshow processes.
   """
-  def hello do
-    :world
+
+  use Supervisor
+
+  def start_link(init_arg) do
+    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
   end
 end
